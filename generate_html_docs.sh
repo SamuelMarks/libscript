bd154266506fb9121488346b645048d1b39198af
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-02-06 22:42:43 -0500

    [generate_html_docs.sh] Improve HTML docs generation (drastically!)

--- generate_html_docs.sh
+++ generate_html_docs.sh
@@ -24,9 +24,16 @@ export LIBSCRIPT_ROOT_DIR
 LIBSCRIPT_DOCS_DIR="${LIBSCRIPT_DOCS_DIR:-./docs}"
 export LIBSCRIPT_DOCS_DIR
 
-set +f
+HTML_ROOT="${HTML_ROOT:-$LIBSCRIPT_ROOT_DIR}"
+export HTML_ROOT
+
 [ -d "${LIBSCRIPT_DOCS_DIR}" ] || mkdir -p "${LIBSCRIPT_DOCS_DIR}"
-for f in $(find "${LIBSCRIPT_ROOT_DIR}" -type f -name 'README.md'); do
+cp 'tuicss.min.css' "${LIBSCRIPT_DOCS_DIR}"'/'
+cp 'tuicss.min.js' "${LIBSCRIPT_DOCS_DIR}"'/'
+cp 'styles.css' "${LIBSCRIPT_DOCS_DIR}"'/'
+urls_js='['
+urls=''
+for f in $(find -s "${LIBSCRIPT_ROOT_DIR}" -type f -name '*.md'); do
   out="${LIBSCRIPT_DOCS_DIR}${f#"${LIBSCRIPT_ROOT_DIR}"}"
   parent="$(dirname -- "${out}")"
 
@@ -52,8 +59,38 @@ for f in $(find "${LIBSCRIPT_ROOT_DIR}" -type f -name 'README.md'); do
       [ -d "${parent}" ] || mkdir -p "${parent}"
       ;;
   esac
-  pandoc -f markdown -t html5 "${f}" -o "${out%%.md}"'.html'
+  # npm install -g @adobe/jsonschema2md
+
+  html="${out%%.md}"'.html'
+
+  cp "${HTML_ROOT}"'/top.html' "${html}"
+  iconv -t utf-8 "${f}" | sed 's/.md)/.html)/g' | pandoc -f markdown -t html5 | iconv -f utf-8 >> "${html}"
+  json_schema="${out%%.md}"'.schema.json'
+  if [ -f "${json_schema}" ]; then
+    true  # TODO
+  fi
+  cat -- "${HTML_ROOT}"'/bottom.html' >> "${html}"
+  urls_js="${urls_js}"'"'"${html#.}"'",'
+  urls="${urls}"' '"${html}"
 done
+urls_js="${urls_js%,}"']'
+printf '%s\n' "${urls_js}"
+
+#tmp_script="$(mktemp)"
+SCRIPT_NAME="${LIBSCRIPT_ROOT_DIR}"'/_lib/_common/envsubst_safe.sh'
+#printf '#!/bin/sh\n export SCRIPT_NAME ; . "%s" ; envsubst_safe' "${SCRIPT_NAME}" > "${tmp_script}"
+#chmod +x "${tmp_script}"
+export SCRIPT_NAME
+# shellcheck disable=SC1090
+. "${SCRIPT_NAME}"
+for url in ${urls}; do
+  # env -i tmp_script="${tmp_script}" url="${url}" REPLACE_THIS="${urls_js}" "${tmp_script}" < "${url}" > "${url}"'.tmp'
+  REPLACE_THIS="${urls_js}"
+  export REPLACE_THIS
+  envsubst < "${url}" > "${url}"'.tmp'
+  mv "${url}"'.tmp' "${url}"
+done
+#rm -- "${tmp_script}"
 ##for d in "${LIBSCRIPT_ROOT_DIR}"'/_lib/'*; do
  # echo "${d%LIBSCRIPT_ROOT_DIR}"
 #done
