0bb0d90593f374818044cca2725603c57b59432b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-02-13 14:00:56 -0700

    [generate_html_docs.sh] Further attempts at getting json schema to work ; [top.html] Improve names in sidebar

--- generate_html_docs.sh
+++ generate_html_docs.sh
@@ -77,12 +77,8 @@ while IFS= read -r f; do
       case "${json_schema##*/}" in
         'installer.schema.json'|'*'*) ;;
         *)
-          [ -d "${f%/*}" ] || mkdir -p -- "${f%/*}"
-          [ -d "${html%/*}" ] || mkdir -p -- "${html%/*}"
-          touch -- "${html}"
-          printf 'processing schema %s and appending to %s\n' "${f%/*}"'/'"${json_schema}" "${html}"
-          wetzel --headerLevel 2 -k '**MUST**' -- "${f%/*}"'/'"${json_schema}" | iconv -t utf-8 \
-            | pandoc -f markdown -t html5 | iconv -f utf-8 >> "${html}"
+          printf 'processing schema %s and appending to %s\n\n' "${f%/*}"'/'"${json_schema}" "${html}"
+          iconv -t utf-8 -- "${json_schema}" | wetzel --headerLevel 2 -k '**MUST**' | pandoc -f markdown -t html5 | iconv -f utf-8 >> "${html}"
           ;;
       esac
       # npm install -g wetzel
